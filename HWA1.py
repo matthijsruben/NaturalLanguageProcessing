7fd589bca2796a5083d43899c3a649144eeaa569
s1997807 <m.r.kok@student.utwente.nl>
2020-09-07 13:16:01 +0200

    Exercise 2

--- HWA1.py
+++ HWA1.py
@@ -3,10 +3,44 @@ from nltk.stem import PorterStemmer
 # create an object of class PorterStemmer
 porter = PorterStemmer()
 
-print(porter.stem("cats"))
-print(porter.stem("trouble"))
-print(porter.stem("troubling"))
-print(porter.stem("troubled"))
+print("SORTED_TYPES.txt")
+file = open("sorted_types.txt")
+words_list = file.readlines()
 
-print(porter.stem("called"))
-print(porter.stem("call"))
+stemmed_list = []
+for word in words_list:
+    stemmed_list.append(porter.stem(word[:-1]))
+
+number_types = 0
+previous = ""
+for stem in stemmed_list:
+    if stem != previous:
+        number_types += 1
+        previous = stem
+
+print("Amount of types (unique words): " + str(number_types))
+print("Original word list length: " + str(len(words_list)))
+
+print("\n")
+print("Words from Exercise 1")
+file = open("Exercise1_words.txt")
+words_list = file.readlines()
+
+stemmed_list = []
+for word in words_list:
+    stemmed_list.append(porter.stem(word[:-1]))
+
+number_types = 0
+previous = ""
+for stem in stemmed_list:
+    if stem != previous:
+        number_types += 1
+        previous = stem
+
+print("Amount of types (unique words): " + str(number_types))
+print("Original word list length: " + str(len(words_list)))
+
+print(words_list)
+print(stemmed_list)
+
+
