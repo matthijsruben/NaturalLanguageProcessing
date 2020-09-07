52ed78a3e83f51769b97b131f8fa3954b806dc5a
s1997807 <m.r.kok@student.utwente.nl>
2020-09-07 11:41:37 +0200

    created a PorterStemmer object

--- HWA1.py
+++ HWA1.py
@@ -1,1 +1,10 @@
-# Hellooo
+from nltk.stem import PorterStemmer
+
+# create an object of class PorterStemmer
+porter = PorterStemmer()
+
+print(porter.stem("cats"))
+print(porter.stem("trouble"))
+print(porter.stem("troubling"))
+print(porter.stem("troubled"))
+
