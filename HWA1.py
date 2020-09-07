091850e6fdd9b40bfbe78b45b1c749c0569eafa5
s1997807 <m.r.kok@student.utwente.nl>
2020-09-07 17:52:40 +0200

    added Exercise 3

--- HWA1.py
+++ HWA1.py
@@ -1,8 +1,12 @@
 from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+# import nltk
+# nltk.download('punkt')  # apparently this was needed for word_tokenize library
 
 # create an object of class PorterStemmer
 porter = PorterStemmer()
 
+# SORTED_TYPES.txt ----------------------------------------------
 print("SORTED_TYPES.txt")
 file = open("sorted_types.txt")
 words_list = file.readlines()
@@ -20,6 +24,11 @@ for stem in stemmed_list:
 
 print("Amount of types (unique words): " + str(number_types))
 print("Original word list length: " + str(len(words_list)))
+
+print(words_list)
+print(stemmed_list)
+
+# Exercise1_words.txt ----------------------------------------------
 
 print("\n")
 print("Words from Exercise 1")
@@ -43,4 +52,32 @@ print("Original word list length: " + str(len(words_list)))
 print(words_list)
 print(stemmed_list)
 
+# Chapter2FirstPage.txt ----------------------------------------------
+
+print("\n")
+print("Words of Chapter2FirstPage.txt")
+
+# I removed the " character that was copied from the pdf from the text and replaced it with " in the text file
+# I removed the ' character that was copied from the pdf from the text and replaced it with ' in the text file
+# It otherwise gave an error
+# The contents of the first page of CH2 were copied to a txt.file
+file = open("Chapter2FirstPage.txt")
+words_list = file.readlines()
+
+
+def stem_sentence(sentence):
+    token_words = word_tokenize(sentence)
+    stemmed_sentence = []
+    for element in token_words:
+        stemmed_sentence.append(porter.stem(element))
+    return " ".join(stemmed_sentence)
+
+
+stemmed_piece_of_text = ""
+for sentence in words_list:
+    stemmed_sentence = stem_sentence(sentence)
+    stemmed_piece_of_text += stemmed_sentence
+
+print("FINAL")
+print(stemmed_piece_of_text)
 
