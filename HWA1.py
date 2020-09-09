c48d94da834cf334dea8f515b29c7155facd6a3d
s1997807 <m.r.kok@student.utwente.nl>
2020-09-09 21:04:15 +0200

    Made lemmatizer instead of stemmer for ex3

--- HWA1.py
+++ HWA1.py
@@ -1,7 +1,9 @@
 from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import sent_tokenize, word_tokenize
 # import nltk
 # nltk.download('punkt')  # apparently this was needed for word_tokenize library
+# nltk.download('wordnet') # apparently this was needed for WordNetLemmatizer library
 
 # create an object of class PorterStemmer
 porter = PorterStemmer()
@@ -73,22 +75,35 @@ def stem_sentence(sentence):
     return " ".join(stemmed_sentence), stemmed_sentence
 
 
+wordnet_lemmatizer = WordNetLemmatizer()
+
+
+def lemmatize_sentence(sentence):
+    token_words = word_tokenize(sentence)
+    lemmatized_sentence = []
+    for element in token_words:
+        lemmatized_sentence.append(wordnet_lemmatizer.lemmatize(element, pos="v"))
+    return " ".join(lemmatized_sentence), lemmatized_sentence
+
+
+punctuations = "?:!.,;"
 stemmed_piece_of_text = ""
-list_of_all_stems = []
+list_of_all_lemmas = []
 for sentence in words_list:
-    stemmed_sentence = stem_sentence(sentence)
+    stemmed_sentence = lemmatize_sentence(sentence)
     stemmed_piece_of_text += stemmed_sentence[0]
     for token in stemmed_sentence[1]:
-        list_of_all_stems.append(token)
+        if token not in punctuations:
+            list_of_all_lemmas.append(token)
 
 print(stemmed_piece_of_text)
-print(list_of_all_stems)
-print("Amount of tokens: ", len(list_of_all_stems))
+print(list_of_all_lemmas)
+print("Amount of tokens: ", len(list_of_all_lemmas))
 
-amount_tokens = len(list_of_all_stems)
-for i in range(0, len(list_of_all_stems)):
+amount_tokens = len(list_of_all_lemmas)
+for i in range(0, len(list_of_all_lemmas)):
     for k in range(0, i):
-        if list_of_all_stems[i] == list_of_all_stems[k]:
+        if list_of_all_lemmas[i] == list_of_all_lemmas[k]:
             amount_tokens -= 1
             break
 print("Amount of types (unique tokens): ", amount_tokens)
